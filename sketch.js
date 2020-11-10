ccabecc5a56866f56c1e68dd9a0e24792a60c434
S90756 <sparas2005@gmail.com>
2020-11-10 17:53:23 +0100

    Crazy Angry Bird Sling

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -23,6 +25,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    constraintLog= new Log(230,180,80,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -35,7 +38,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain= new Chain(bird.body,constraintLog.body)
 }
 
 function draw(){
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constraintLog.display()
+    chain.display();
 }
